3992fb537be1a6b4d9da13d402da84e7ad2e4412
Dhomini  Rabelo <dhominisalgado@gmail.com>
2021-12-08 16:09:52 -0300

    app posts editado por django helper

--- project/BLOG/settings.py
+++ project/BLOG/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # My apps
+    'posts.PostsConfig',
     'accounts.AccountsConfig',
     # Others apps
 ]
--- project/posts/models.py
+++ project/posts/models.py
@@ -1,3 +1,3 @@
-from django.db import models
+from django.db.models import (Model, CharField, DateTimeField, TextField, EmailField, ForeignKey, PositiveIntegerField, ImageField, RESTRICT, DecimalField, DateField, BooleanField)
 
 # Create your models here.
