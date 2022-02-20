6dfce4ee1f16d369699bc538649f8e89c60fbba2
Dhomini Fael <dhominisalgado@gmail.com>
2022-02-19 22:23:14 -0300

    criando model e api de sugestões

--- project/suggestions/admin.py
+++ project/suggestions/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(CategorySuggestion)
+class CategorySuggestionAdmin(admin.ModelAdmin):
+    list_display = 'name', 'state'
+    list_display_links = 'name',
+    ordering = 'name',
+    search_fields = 'name',
+    
+    
+@admin.register(SubCategorySuggestion)    
+class SubCategorySuggestionAdmin(admin.ModelAdmin):
+    list_display = 'name', 'state'
+    list_display_links = 'name',
+    ordering = 'name',
+    search_fields = 'name',
--- project/suggestions/models.py
+++ project/suggestions/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from django.db.models import (Model, CharField, DateTimeField, TextField, EmailField, ForeignKey, PositiveIntegerField, ImageField, RESTRICT, DecimalField, DateField, BooleanField)
+from accounts.models import User
 
-# Create your models here.
+
+SUGGESTION_STATES = [
+    
+    ('invalid', 'INVÁLIDO'),
+    ('reject', 'RECUSADO'),
+    ('loading', 'EM ANDAMENTO'),
+    ('accept', 'ACEITO'),
+    
+]
+
+
+
+class CategorySuggestion(Model):
+    name = CharField(max_length=50, verbose_name='Nome')
+    state = CharField(max_length=256, choices=SUGGESTION_STATES, verbose_name='Estado:')
+    user = ForeignKey(User, on_delete=RESTRICT, verbose_name='Usuário')
+
+
+class SubCategorySuggestion(Model):
+    name = CharField(max_length=50, verbose_name='Nome')
+    state = CharField(max_length=256, choices=SUGGESTION_STATES, verbose_name='Estado:')
+    user = ForeignKey(User, on_delete=RESTRICT, verbose_name='Usuário')
