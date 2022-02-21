79f94a1a1cb0cdc629284af963901af9fca01da4
Dhomini Fael <dhominisalgado@gmail.com>
2022-02-21 14:16:42 -0300

    error na api e adaptando página de listagem e account-page

--- project/api/urls.py
+++ project/api/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('category-suggestion/view-or-create', CategorySuggestionView.as_view()),
-    path('subcategory-suggestion/view-or-create', CategorySuggestionView.as_view(), name='api_subcategory_suggestion'),
+    path('subcategory-suggestion/view-or-create', SubCategorySuggestionView.as_view(), name='api_subcategory_suggestion'),
     path('token/get-token', TokenObtainPairView.as_view()),
     path('token/refresh-token', TokenRefreshView.as_view()),
     path('token/get-cookie', get_cookie),
