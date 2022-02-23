b41536a0c35eae57b8c6e1ce6caae9e0b1815d23
Dhomini  Rabelo <dhominisalgado@gmail.com>
2022-02-23 01:32:45 -0300

    consertando bug vh e lista de imgs em pcs dinamicos

--- project/Support/Code/actions/_accounts/login_group/login.py
+++ project/Support/Code/actions/_accounts/login_group/login.py
@@ -3,17 +3,17 @@ from random import randint
 from rest_framework_simplejwt.tokens import RefreshToken
 
 def get_aleatory_profile_photo():
-    path = 'C:/Users/G-fire/OneDrive/Documentos/GITHUB/DJANGO/Blog-Django-with-CBV/project/Support/FrontEnd/media/users/default'
+    path = Path.cwd() / Path('Support/FrontEnd/media/users/default')
     current_path = Path(path)
     archives = []
     for archive in current_path.iterdir():
         archives.append(archive.name)
     archives_quantity = len(archives)
     drawn_position = randint(0, archives_quantity-1)
-    chosen_file = archives[drawn_position]
+    chosen_file = archives[drawn_position] 
     return f'users/default/{chosen_file}'
     
-
+get_aleatory_profile_photo()
 
 def get_token_for_user(request):
     refresh = RefreshToken.for_user(request.user)
