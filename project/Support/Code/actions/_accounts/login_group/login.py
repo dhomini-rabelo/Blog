f4b415ec3751660dd0ff223697bed817bc9a19ec
Dhomini  Rabelo <dhominisalgado@gmail.com>
2022-02-26 02:41:27 -0300

    minificando js sem type="module" e lista manual de fotos para users

--- project/Support/Code/actions/_accounts/login_group/login.py
+++ project/Support/Code/actions/_accounts/login_group/login.py
@@ -3,11 +3,40 @@ from random import randint
 from rest_framework_simplejwt.tokens import RefreshToken
 
 def get_aleatory_profile_photo():
-    path = Path.cwd() / Path('Support/FrontEnd/media/users/default')
-    current_path = Path(path)
-    archives = []
-    for archive in current_path.iterdir():
-        archives.append(archive.name)
+    archives = [
+        'aviao.png',
+        'bandeira.png',
+        'bitcoin.png',
+        'cha.png',
+        'chuva.png',
+        'computador.png',
+        'controle.png',
+        'coquetel.png',
+        'euro.png',
+        'floco-de-neve.png',
+        'foguete.png',
+        'foto.py',
+        'globo-terrestre.png',
+        'hamburger.png',
+        'lua.png',
+        'luva.png',
+        'mao.png',
+        'medalha-de-bronze.png',
+        'medalha-de-ouro.png',
+        'medalha-de-prata.png',
+        'moon.png',
+        'oculos-escuros.png',
+        'pizza.png',
+        'presente.png',
+        'relampago.png',
+        'rocketseat.png',
+        'servidor.png',
+        'simbolo-do-dolar.png',
+        'sol.png',
+        'ufo.png',
+        'xicara-de-cafe.png',
+    ]
+
     archives_quantity = len(archives)
     drawn_position = randint(0, archives_quantity-1)
     chosen_file = archives[drawn_position] 
