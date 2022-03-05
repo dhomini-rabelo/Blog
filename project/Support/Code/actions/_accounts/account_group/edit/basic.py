3413c23bf4fdc1232c011a3ae6fd864d866d0ea3
Dhomini Fael <dhominisalgado@gmail.com>
2022-03-05 16:24:59 -0300

    slug únicoo se usuário for autor e usando get_name que é melhor que .title()

--- project/Support/Code/actions/_accounts/account_group/edit/basic.py
+++ project/Support/Code/actions/_accounts/account_group/edit/basic.py
@@ -1,11 +1,20 @@
+from Support.Code.actions.Support.utils.functions_dict import get_name
+from django.utils.text import slugify
+
+
 def save_user_basic_and_update_user_save(request):
     user = request.user
     name: str = request.POST.get('name')
     new_file = request.FILES.get('photo')
     if new_file:
         user.photo = new_file
-    user.name = name.title()
+    user.name = get_name(name)
+    new_slug = slugify(name)
+    if request.session['user_save']['data']['author']:
+        new_slug = f'{new_slug}-{user.id}'
+    user.slug = new_slug
     user.save()
-    request.session['user_save']['name'] = name.title()
-    request.session['user_save']['photo_path'] = user.photo.url
+    request.session['user_save']['data']['name'] = get_name(name)
+    request.session['user_save']['data']['slug'] = new_slug
+    request.session['user_save']['data']['photo_url'] = user.photo.url
     request.session.save()
